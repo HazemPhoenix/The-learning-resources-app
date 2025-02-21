5ed3eb1b358822500aace233fd257dd53b052d8f
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2025-02-21 17:54:36 +0200

    Add the ResourceItem component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
-<script></script>
-
 <template>
-  <h1 class="text-blue-500">Hello, world!</h1>
+  <ul class="flex flex-col items-center gap-3 border-1 w-8/12 mx-auto">
+    <ResourceItem v-for="item in learningResources" :item="item" :key="item.id" />
+  </ul>
 </template>
+
+<script>
+import ResourceItem from "./components/ResourceItem.vue";
+export default {
+  components: {
+    ResourceItem,
+  },
+  data() {
+    return {
+      learningResources: [
+        {
+          id: 1,
+          title: "Vue.js official docs",
+          description: "The official documentation for Vue.js",
+          link: "https://vuejs.org",
+        },
+        {
+          id: 2,
+          title: "Maxmilian Schwarzmuller Vue.js course",
+          description: "The udemy course on Vue.js by Maxmilian Schwarzmuller",
+          link: "https://www.udemy.com/course/vuejs-2-the-complete-guide/",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/ResourceItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li
+    class="flex flex-col justify-between w-full rounded overflow-hidden shadow-lg bg-white hover:shadow-xl transition-shadow duration-300"
+  >
+    <div class="px-6 py-4">
+      <h2 class="font-bold text-xl mb-2 text-gray-800">{{ item.title }}</h2>
+      <h3 class="text-gray-700 text-base">
+        {{ item.description }}
+      </h3>
+    </div>
+    <div class="px-6 pt-4 pb-6">
+      <a
+        :href="item.link"
+        target="_blank"
+        class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition-colors duration-300"
+      >
+        Visit
+      </a>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+};
+</script>
